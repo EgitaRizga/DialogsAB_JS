37b6e62e98e0c01a03a334dc2ac749096efc3684
EgitaRizga <130855640+EgitaRizga@users.noreply.github.com>
2023-04-29 19:06:45 +0300

    if_else

--- 06_nodarbiba/js/else.js
+++ 06_nodarbiba/js/else.js
@@ -10,14 +10,21 @@ if (hour > 18) {
 
 console.log(greeting);
 
-if (hour > 18) {
+if (hour < 18) {
     //greeting = "Good";
-    greeting = greeting + "day";}
-   else {
-   // greeting = "Good";
-    greeting = greeting + "evening"
+    //console.log(greeting);
+    greeting = greeting + "day";
+    console.log(greeting);
+  } else {
+    //greeting = "Good";
+    greeting = greeting + "evening";
+    if (hour > 22) {
+      greeting = greeting + " (it actually is late evening :-))";
+    } else {
+      greeting = greeting + " (it is not late evening yet :-))";
+    }
   }
-
+  
 console/log(greeting);
 
 document.getElementById("demo_else").innerHTML = greeting;
