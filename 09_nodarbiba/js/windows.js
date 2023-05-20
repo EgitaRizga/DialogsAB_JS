c7f8a123755d4b074cba5748558cade38fbd3d1b
EgitaRizga <130855640+EgitaRizga@users.noreply.github.com>
2023-05-20 22:29:53 +0300

    09_10_nodarbiba

--- 09_nodarbiba/js/windows.js
+++ 09_nodarbiba/js/windows.js
@@ -49,3 +49,48 @@ https://www.javascripttutorial.net/javascript-bom/javascript-window/
 
 
 */
+
+
+
+//let timeoutID  = setTimeout(cb [,delay], arg1, arg2,...);
+
+
+let intervalID;
+ 
+function toggleColor() {
+  let e = document.getElementById('flashtext');
+  e.style.color = e.style.color == 'red' ? 'blue' : 'red';
+}
+
+function stop() {
+  clearInterval(intervalID);
+}
+
+
+function start() {
+   intervalID = setInterval(toggleColor, 1000); 
+}
+
+
+URL = 'https://letonika.lv/groups/default.aspx?g=5&r=1100'
+urlParamsString = ULR; //location.search
+urlParams = new URLSearchParams(urlParamsString);
+
+
+for (const [key, value] of urlParams) {
+    console.log(`${key}:${value}`);
+}
+
+for (const key of urlParams.keys()) {
+    console.log(key);
+}
+
+
+for (const value of urlParams.values()) {
+    console.log(value);
+}
+
+for (const entry of urlParams.entries()) {
+    console.log(entry);
+}
+
---
+++ 10_nodarbiba/js/document.js
@@ -0,0 +1,3 @@
+
+const hello = document.getElementById('hello');
+console.log(hello);
